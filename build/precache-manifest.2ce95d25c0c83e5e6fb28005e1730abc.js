9c45c3384f8f2c4bb4d1522abf41a374806173d5
Justin Diala <justin@MOON.MOON>
2020-05-13 21:38:14 -0600

    Added the basic react modules

---
+++ build/precache-manifest.2ce95d25c0c83e5e6fb28005e1730abc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea74026c7b25b480886d6834b2e4e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7726d75458ddaefb555e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e328d9a40eb3e51f0f7",
+    "url": "/static/js/2.49acddc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49acddc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7726d75458ddaefb555e",
+    "url": "/static/js/main.fd6d2576.chunk.js"
+  },
+  {
+    "revision": "9c94aa92516ff1428587",
+    "url": "/static/js/runtime-main.d943b06e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
